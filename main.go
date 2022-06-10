6a9840fdfcf1661f82f6eb873184f2b995533fd9
Mikkel Birkedam <mikkelsteen@gmail.com>
2022-06-10 15:27:49 +0200

    big changes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -16,6 +17,8 @@ var sq_config config.RepositoryConfig
 var db *SQLite
 
 func init() {
+
+	// To do: Make config relative
 
 	var err error
 	cfg, err := config.Read("/Users/mikkel/feedfetcher/config.json")
@@ -45,9 +48,65 @@ func main() {
 	resources_fileserver := http.FileServer(http.Dir("./resources"))
 
 	http.HandleFunc("/", listArticles)
+	http.HandleFunc("/feed/", listArticlesJson)
+	http.HandleFunc("/test/", listArticlesTest)
 	http.HandleFunc("/resources/", http.StripPrefix("/resources", resources_fileserver).ServeHTTP)
 	http.ListenAndServe(":3001", nil)
 
+}
+
+func listArticlesTest(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(404)
+	}
+
+	languages, ok := r.Form["lang"]
+
+	if !ok {
+		languages = make([]string, 0)
+	}
+
+	var p Page
+	p.setChecked(languages)
+
+	p.Title = "HeadlineTracker"
+	p.Today = time.Now().Format(time.ANSIC)
+	p.Articles = getLatestArticles(20, languages)
+
+	t, _ := template.ParseFiles(
+		"./html_templates/list_test.html",
+		"./html_templates/article.html",
+		"./html_templates/header.html",
+		"./html_templates/dropdown.html",
+	)
+
+	t.Execute(w, p)
+}
+
+func listArticlesJson(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		w.WriteHeader(404)
+	}
+
+	languages, ok := r.Form["lang[]"]
+	if !ok {
+		languages = make([]string, 0)
+	}
+
+	fmt.Println(r.Form)
+	fmt.Println(languages)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Encoding", "utf-8")
+	articles := getLatestArticles(20, languages)
+	jsondata, err := json.Marshal(articles)
+	if err != nil {
+		w.WriteHeader(404)
+	}
+	w.Write(jsondata)
 }
 
 func listArticles(w http.ResponseWriter, r *http.Request) {
--- resources/custom.js
+++ resources/custom.js
@@ -1,23 +1,39 @@
-$(function () {
+
+
+// Hide the checkboxes.
+// We only need to see them when "filters" has been pressed.
+$(document).ready(function () {
     $('div.checkboxes').hide();
 });
 
-
-$( "#filter" ).submit(function( event ) {
-    alert( "Handler for .submit() called." );
-    event.preventDefault();
-  });
-
+// Toggle hide/visible when pressing the filter button
 
 $(document).ready(function () {
     $("p").click(function () {
         if ($('div.checkboxes').is(':visible')) {
-            $("div.checkboxes").slideToggle();
-            $("#filter").submit();
+            $("div.checkboxes").slideToggle()
+            var boxes = []
+            $("input:checkbox:checked").each(function() {
+                boxes.push($(this).attr("value"))
+            })
+            $.post('/feed/', {"lang": boxes}, function(data) {
+
+                var articles = $("div.article", "div.article_list")
+                for (a in data) {
+                    new_headline = '<a href="' + data[a].url + '">▤ ' + data[a].headline + "/>"
+                    new_story = data[a].source + ": " + data[a].headline
+                    new_publish_date = "Published: " + data[a].docdate
+                    console.log(new_publish_date)
+
+                    articles.eq(a).attr("id", data[a].id)
+                    articles.eq(a).find("div.headline").html(new_headline)
+                    articles.eq(a).find("div.story").html(new_story)
+                    articles.eq(a).find("div.date").html(new_publish_date)
+                }
+            })
+            //$("#filter").submit();
         } else {
             $("div.checkboxes").slideToggle();
         }
     });
 });
-
-
